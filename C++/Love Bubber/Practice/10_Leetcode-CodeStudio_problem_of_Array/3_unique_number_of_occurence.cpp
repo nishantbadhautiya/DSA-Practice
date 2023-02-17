830b74f5074e6f2f1e0e062f920e3cf67b771504
Nishant Badhautiya <nishantbadhautiya100@gmail.com>
2023-02-17 20:48:49 +0530

    added more dsa practice work

---
+++ C++/Love Bubber/Practice/10_Leetcode-CodeStudio_problem_of_Array/3_unique_number_of_occurence.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+bool uniqueOccurrences(int arr[], int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 2, 1, 1, 3};
+    bool result = uniqueOccurrences(arr, sizeof(arr) / sizeof(int));
+    return 0;
+}
